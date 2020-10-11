a81e893a5734c58ff36df1f01dd5fe5697d99934
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-10-11 23:21:24 +0200

    router and static files

--- 03expressnodejs/app.js
+++ 03expressnodejs/app.js
@@ -6,8 +6,18 @@ const data = require('./data');
 
 router.get('/employees', (req, res) => res.send(data));
 
+router.get('/employees/:id', (req, res) => {
+    const id = +req.params.id;
+    const employee = data.filter(d => d.id === id);
+    res.send(employee);
+});
+
 // router.get('/employees', (req, res) => res.send('Hello World!'));
 app.use('/api', router)
+    // app.use(express.static('images'));
+
+app.use('/static', express.static('images'));
+
 
 const port = 3000;
 // app.get('/api/employees', (req, res) => res.send('Hello World!'));
---
+++ 03expressnodejs/readme.md
@@ -0,0 +1,9 @@
+# static files
+
+<http://localhost:3000/api/employees>
+
+<http://localhost:3000/api/employees/1>
+
+to call static files
+
+<http://localhost:3000/fullstack.jpg>
