1197fb21cd709e4209df20cc89b05910ee0e74ae
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-10-11 18:54:49 +0200

    advance routing via router object

--- 03expressnodejs/app.js
+++ 03expressnodejs/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+
+const data = require('./data');
+
+router.get('/employees', (req, res) => res.send(data));
+
+// router.get('/employees', (req, res) => res.send('Hello World!'));
+app.use('/api', router)
 
 const port = 3000;
-app.get('/api/employees', (req, res) => res.send('Hello World!'));
-app.post('/api/employees', (req, res) => res.send('Http POST in action'));
+// app.get('/api/employees', (req, res) => res.send('Hello World!'));
+// app.post('/api/employees', (req, res) => res.send('Http POST in action'));
 
 app.listen(port,
     () => console.info(`server is listening on ${port}`));
